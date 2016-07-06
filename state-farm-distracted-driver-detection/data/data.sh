14678290e3efb586d3d245b5e272e6c0528ef61d
吕一 <lvyi@weidian.com>
2016-07-06 18:53:58 +0800

    state-farm-distracted-driver-detection

---
+++ state-farm-distracted-driver-detection/data/data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget "https://kaggle2.blob.core.windows.net/competitions-data/kaggle/5048/imgs.zip?sv=2012-02-12&se=2016-07-09T10%3A51%3A39Z&sr=b&sp=r&sig=Srs2AyVDe8A%2BLYgfeT5OEAQqtceGNN97lV5Bg6OGq48%3D" -O imgs.zip
